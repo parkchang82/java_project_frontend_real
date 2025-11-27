1abae47484581f96b61d8adc2fba565884df4b71
parkchang82 <5847park@gmail.com>
2025-11-28 01:22:30 +0900

    go

--- project-frontend/src/api/api.js
+++ project-frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://java-project-backend-real.onrender.com",
   withCredentials: true,
 });
 
